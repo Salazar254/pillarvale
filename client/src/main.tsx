8f4ca7a46416ba6cb6c92407f6b534fa643be79e
Salazar254 <asherkiiru777@gmail.com>
2026-02-04 17:09:25 +0300

    Allow app to load without Clerk key for visibility on Vercel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,16 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.tsx'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-    throw new Error("Missing Publishable Key")
-}
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || ""
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        {PUBLISHABLE_KEY ? (
+            <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+                <App />
+            </ClerkProvider>
+        ) : (
             <App />
-        </ClerkProvider>
+        )}
     </StrictMode>,
 )
